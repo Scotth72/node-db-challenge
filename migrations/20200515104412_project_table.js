a65eb06fe29fae854d8f971bee73e7001497d071
Heath Scott <HeathScott@Heaths-MacBook-Pro.local>
2020-05-15 11:17:21 -0500

    setup some tables

--- migrations/20200515104412_project_table.js
+++ migrations/20200515104412_project_table.js
@@ -1,10 +1,28 @@
 exports.up = function(knex) {
-	return knex.schema.createTable('projects', (tbl) => {
-		tbl.increments('id');
-		tbl.string('name', 128).notNullable().unique();
-		tbl.string('description', 256);
-		tbl.boolean('complete').defaultTo(false);
-	});
+	return knex.schema
+		.createTable('projects', (tbl) => {
+			tbl.increments('id');
+			tbl.string('name', 128).notNullable().unique();
+			tbl.string('description', 256);
+			tbl.boolean('complete').defaultTo(false);
+		})
+		.createTable('resources', (tbl) => {
+			tbl.increments();
+			tbl.string('name').notNullable().unique();
+			tbl.string('description', 256);
+		})
+		.createTable('tasks', (tbl) => {
+			tbl.increments('id');
+			tbl.string('description', 256).notNullable();
+			tbl.string('notes', 256);
+			tbl
+				.integer('project_id')
+				.notNullable()
+				.references('id')
+				.inTable('projects')
+				.onDelete('CASCADE')
+				.onUpdate('CASCADE');
+		});
 };
 
 exports.down = function(knex) {};
